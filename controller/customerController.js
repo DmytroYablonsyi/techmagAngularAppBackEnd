3f427d60608a059f545c9b1014651bf7c86afaf6
Dmytro Yablonskiy <dmytroyablonskiy@MacBook-Air-Dmytro.local>
2025-02-15 17:45:15 +0200

    added edit and delete functionality

--- controller/customerController.js
+++ controller/customerController.js
@@ -31,4 +31,72 @@ const createCustomer = asyncHandler (async (req, res) => {
 
 });
 
-export { getCustomers, createCustomer}
+// get customer by id
+// get /api/customers/:id
+
+const getCustomerById = asyncHandler (async (req, res) => {
+    try {
+        const customerId = req.params.id;
+    
+        const customer = await Customer.findById(customerId);
+    
+        if (!customer) {
+          return res.status(404).json({ message: 'Customer not found' });
+        }
+    
+        res.status(200).json(customer);
+      } catch (error) {
+        console.error('Error fetching customer:', error);
+        res.status(500).json({ message: 'Server error' });
+      }
+})
+
+// update customer
+// put /api/customers/:id
+
+const updateCustomer = asyncHandler (async (req, res) => {
+    const id = req.params.id;
+    const { name, contactPerson, city, street, phone } = req.body
+    const newData = {
+        name: name,
+      address: {
+        city: city,
+        street: street
+      },
+      phone: phone,
+      contactPerson: contactPerson
+    }
+
+    try {
+        const updatedCustomer = await Customer.findByIdAndUpdate(id, newData, { new: true });
+        if (!updatedCustomer) {
+          return res.status(404).send('Client not found');
+        }
+        res.status(200).json(updatedCustomer);
+      } 
+    catch (error) {
+        res.status(500).send('Error updating client data');
+      }
+})
+
+// delete customer
+// delete /api/customers/:id
+
+const deleteCustomer = asyncHandler (async (req, res) => {
+    const customerId = req.params.id;
+
+    try {
+      const deletedCustomer = await Customer.findByIdAndDelete(customerId);
+  
+      if (!deletedCustomer) {
+        return res.status(404).json({ message: 'Customer not found' });
+      }
+  
+      res.status(200).json({ message: 'Customer deleted successfully' });
+    } catch (error) {
+      console.error('Error deleting customer:', error);
+      res.status(500).json({ message: 'Server error' });
+    }
+})
+
+export { getCustomers, createCustomer, updateCustomer, getCustomerById, deleteCustomer}
--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 const router = express.Router();
-import { createCustomer, getCustomers } from "../controller/customerController.js";
+import { createCustomer, deleteCustomer, getCustomerById, getCustomers, updateCustomer } from "../controller/customerController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 router.route("/").get(protect, getCustomers).post(protect, createCustomer);
+router.route("/:id").put(protect, updateCustomer).get(protect, getCustomerById).delete(protect, deleteCustomer)
 
 export default router
